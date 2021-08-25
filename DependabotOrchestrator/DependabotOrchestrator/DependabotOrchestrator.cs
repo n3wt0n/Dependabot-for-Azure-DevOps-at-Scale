3218cfa7caa3e3c619a4b7b55c5547638d6d2ecc
Davide Benvegnu <davide.benvegnu@outlook.com>
2021-08-25 16:45:52 +0800

    Cleanup

--- DependabotOrchestrator/DependabotOrchestrator/DependabotOrchestrator.cs
+++ DependabotOrchestrator/DependabotOrchestrator/DependabotOrchestrator.cs
@@ -21,29 +21,7 @@ using DependabotOrchestrator.Extensions;
 namespace DependabotOrchestrator
 {
     public static class DependabotOrchestrator
-    {
-        
-
-        //[FunctionName("Function1")]
-        //public static async Task<IActionResult> Run(
-        //    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        //    ILogger log)
-        //{
-        //    log.LogInformation("C# HTTP trigger function processed a request.");
-
-        //    string name = req.Query["name"];
-
-        //    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        //    dynamic data = JsonConvert.DeserializeObject(requestBody);
-        //    name = name ?? data?.name;
-
-        //    string responseMessage = string.IsNullOrEmpty(name)
-        //        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-        //        : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-        //    return new OkObjectResult(responseMessage);
-        //}
-
+    {                
         [FunctionName("Orchestrator_HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestMessage req,
