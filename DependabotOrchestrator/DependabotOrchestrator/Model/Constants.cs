c8d0994fb894981d497da0e9fbea277f309970b2
Davide Benvegnu <davide.benvegnu@outlook.com>
2021-09-22 17:08:01 +0800

    Added output and error partial redirection to file for Prod container

--- DependabotOrchestrator/DependabotOrchestrator/Model/Constants.cs
+++ DependabotOrchestrator/DependabotOrchestrator/Model/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string DefaultContainerGroupName = "DependabotRunner";
+        public const string DefaultContainerGroupName = "dpbrunner";
         public const string ContainerImageName = "n3wt0n/dependabot-azuredevops-atscale";
         public const string TestContainerImageName = "n3wt0n/dependabot-azuredevops-atscale-testimage";
         public const string DefaultContainerImageTag = "latest";
